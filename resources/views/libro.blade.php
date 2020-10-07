0ad3e0f51199032956764c7781a4729674afe77a
SebastianReyes22 <17151304@aguascalientes.tecnm.mx>
2020-10-07 12:47:38 -0500

    se agrego el link del login/singin

--- resources/views/libro.blade.php
+++ resources/views/libro.blade.php
@@ -1,1 +1,20 @@
-traer de la base de datos el libro seleccionado
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="../public/img/icon.png" type="image/png" />
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>eBookShop</title>
+        <!-- Styles -->
+        <link href="{{ asset( 'css/app.css' ) }}" rel="stylesheet">
+        <link href="{{ asset( 'css/styles.css' ) }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="nose"></div>
+        <script src="{{ asset( 'js/nose.js' ) }}"></script>
+    </body>
+</html>
