a8b8f284ade4e159dcc3e301193051253ad0f484
Sebastian <17151304@aguascalientes.tecnm.mx>
2020-11-03 17:58:45 -0600

    Login del panel de admin, falta diseño

---
+++ Panel de Admin/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+</head>
+<body>
+    <form action="validacion.php" method="post">
+        <h1>Login al panel de administrador</h1>
+        
+        <p>Usuario <input type="email" placeholder="admin@gmail.com" name="usuario"> </p>
+        
+        <p>Contraseña <input type="password" placeholder="contraseña" name="contraseña"> </p>
+        
+        <input type="submit" value="ingresar">
+    </form>
+</body>
+</html>
---
+++ Panel de Admin/validacion.php
@@ -0,0 +1,29 @@
+<?php
+
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+
+session_start();
+
+$_SESSION['usuario']=$usuario;
+
+include('db.php');
+
+$consulta="SELECT*FROM usuarios WHERE correo='$usuario' AND contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas)
+{
+    header("location:Admin/index3.php");
+}else{
+    ?>
+    <?php
+    include("index.php");
+    ?>
+    <h1>ERROR USUARIO INCORRECTO</h1>
+    <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
