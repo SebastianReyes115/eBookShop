936fddd4dde6bc1a8e9dcee6d00301fd66b2ef60
Sebastian <17151304@aguascalientes.tecnm.mx>
2020-11-04 23:21:04 -0600

    Calendario

--- Panel de Admin/Admin/index.php
+++ Panel de Admin/Admin/index.php
@@ -49,25 +49,47 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-         
           <li class="nav-header">Administrar</li>
+          
           <li class="nav-item">
-            <a href="pages/productos.html" class="nav-link">
-              <i class="nav-icon fas icofont-book"></i>
-                            <p>
-                Productos
-                <span class="badge badge-info right">2</span>
-              </p>
+            <a href="pages/calendar.html" class="nav-link">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>Calendario</p>
             </a>
           </li>
-          
+
+          <li class="nav-item">
+            <a href="pages/productos.html" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+                <p>Productos</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/productos.html" class="nav-link">
+              <i class="nav-icon fas fa-chart-pie"></i>
+                <p>Estádisticas</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/productos.html" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+                <p>Base de Datos</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/productos.html" class="nav-link">
+              <i class="nav-icon fas fa-envelope"></i>
+                <p>Inbox</p>
+            </a>
+          </li>
+
         </ul>
       </nav>
-      <!-- /.sidebar-menu -->
+
     </div>
-    <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
@@ -350,11 +372,8 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2020 Team 6.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0-pre
-    </div>
   </footer>
 </div>
 <!-- ./wrapper -->
