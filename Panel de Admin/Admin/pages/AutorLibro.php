59f6ec3dbc43026ba7198b96e081985854ad2542
Sebastian <17151304@aguascalientes.tecnm.mx>
2020-11-08 13:10:17 -0600

    Correccion de menu

--- Panel de Admin/Admin/pages/AutorLibro.php
+++ Panel de Admin/Admin/pages/AutorLibro.php
@@ -51,7 +51,7 @@
           <li class="nav-header">Administrar</li>
 
           <li class="nav-item">
-            <a href="pages/AutorLibro.html" class="nav-link">
+            <a href="AutorLibro.php" class="nav-link">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-chevron-contract" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M3.646 13.854a.5.5 0 0 0 .708 0L8 10.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zm0-11.708a.5.5 0 0 1 .708 0L8 5.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708z"/>
 </svg>
@@ -77,9 +77,8 @@
           <li class="nav-item">
             <a href="autores.php" class="nav-link">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-</svg>
-
+              <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            </svg>
                 <p>Autores</p>
             </a>
           </li>
--- Panel de Admin/Admin/pages/autores.php
+++ Panel de Admin/Admin/pages/autores.php
@@ -54,7 +54,7 @@
 
           
           <li class="nav-item">
-            <a href="pages/AutorLibro.html" class="nav-link">
+            <a href="AutorLibro.php" class="nav-link">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-chevron-contract" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M3.646 13.854a.5.5 0 0 0 .708 0L8 10.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zm0-11.708a.5.5 0 0 1 .708 0L8 5.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708z"/>
 </svg>
@@ -78,7 +78,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="pages/productos.html" class="nav-link">
+            <a href="#" class="nav-link">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
 </svg>
