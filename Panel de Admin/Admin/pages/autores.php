ee9a59a1a08351a1bd5503e06d421dfec85d6bbd
JOSE LUIS CASTORENA ZAMARRIPA <17151292@aguascalientes.tecnm.mx>
2020-11-08 20:17:01 -0600

    Commit Vistas

--- Panel de Admin/Admin/pages/autores.php
+++ Panel de Admin/Admin/pages/autores.php
@@ -50,14 +50,37 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-header">Administrar Base de Datos</li>
           <li class="nav-item">
+            <a href="autores.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Autor</p>
+            </a>
+          </li>
+          <li class="nav-item">
             <a href="AutorLibro.php" class="nav-link">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-chevron-contract" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M3.646 13.854a.5.5 0 0 0 .708 0L8 10.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zm0-11.708a.5.5 0 0 1 .708 0L8 5.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708z"/>
               </svg>
-              <p>AutorLibro</p>
+              <p>Autor_Libro</p>
             </a>
           </li>
-
+          <li class="nav-item">
+            <a href="Editorial.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-book" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+              </svg>
+              <p>Editorial</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="EditorialLibro.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-book" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+              </svg>
+              <p>Editorial_Libro</p>
+            </a>
+          </li>
           <li class="nav-item">
             <a href="Genero.php" class="nav-link">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-ui-radios-grid" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -67,22 +90,110 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="Editorial.php" class="nav-link">
+            <a href="GeneroLibro.php" class="nav-link">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-book" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
               </svg>
-              <p>Editoriales</p>
+              <p>Genero_Libro</p>
             </a>
           </li>
-
           <li class="nav-item">
-            <a href="autores.php" class="nav-link">
+            <a href="Libros_ISBN.php" class="nav-link">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
               </svg>
-              <p>Autores</p>
+              <p>Libros_ISBN</p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="Libros.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Libros</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="LibrosStock.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Libros Disponibles</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="MasVendidos.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Más Vendidos</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="MasVisitados.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Más Visitados</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="MetodoPago.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Método de pago</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="Ofertas.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Ofertas</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="Recomendaciones.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Recomendaciones</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="Usuarios.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Usuarios</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="Ventas.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Ventas</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="VisitasPagina.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Visitas a la página</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="wishlist.php" class="nav-link">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="nav-icon bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+              <p>Lista de Deseos</p>
+            </a>
+          </li>
+
           <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
               <li class="nav-header">Administracion general</li>
@@ -130,7 +241,7 @@
     </div>
     </section>
 
-    <!-- Main content -->
+    <!-- Form principal -->
     <section class="content">
         <div id="form1" class="shadow-lg p-3 mb-5 bg-white rounded" style="width:50%; margin-left:250px;">
         <form method="post">
@@ -156,6 +267,8 @@
   </div>
   <input type="submit" name="b1" onClick="cargar()" class="btn btn-primary btn-lg btn-block" value="Guardar">
 </form>
+
+    <!-- Tabla de la consulta -->
         </div>
         <br><br>
         <table class="table table-dark">
@@ -166,6 +279,7 @@
                 <td>Nacionalidad</td>
                 <td>Edad</td>
             </tr>
+
         <?php
 $con=mysqli_connect("localhost","root","","ebookshop");
 $obtener="SELECT * FROM autor";
@@ -175,19 +289,16 @@ while($fila=$ejecuta->fetch_assoc())
 ?>
 <tr>
     <td><?php echo $fila['id_autor'] ?> </td>
-
     <td><?php echo $fila['NombreAutor'] ?> </td>
     <td><?php echo $fila['ApellidosAutor'] ?> </td>
     <td><?php echo $fila['Nacionalidad'] ?> </td>
-    <td><?php echo $fila['Edad'] ?> </td>
+    <td><?php echo $fila['Edad']?> </td>
     <td> <div class="container">
-    <?php echo '<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#Modal_' . $fila['id_autor'] . '" id=' . $fila['id_autor'] . '" >Editar</button>' ?>
+    <?php echo '<button type="button" class="btn btn-info btn-m" data-toggle="modal" data-target="#Modal_' . $fila['id_autor'] . '" id=' . $fila['id_autor'] . '" >Editar</button>' ?>
              <!--<?php /*echo *///'<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#Modal" id=' . $row['id'] . '" >Editar</button>' ?>-->
              <div class="modal fade" id="Modal_<?= $fila['id_autor']; ?>" role="dialog">
                 <div class="modal-dialog">
-
                     <div class="modal-content">
-
                         <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         </div>
@@ -234,7 +345,7 @@ while($fila=$ejecuta->fetch_assoc())
     <td>
         <form method="post" id="form_eliminar_<?php  echo $fila['id_autor']?>">
         <input type="hidden" name="eliminar" value="<?php echo $fila['id_autor'];?>" />
-    <input type="submit" value="Eliminar" class="btn btn-danger"/>
+    <input type="submit" value="Eliminar" class="btn btn-danger btn-m"/>
         </form>
 </td>
 </tr>
@@ -247,9 +358,10 @@ if(isset($_POST['eliminar'])){ //Eliminar Autor
     if(mysqli_query($con,$sql2)){
     echo "";
     echo "<div class='alert alert-warning' role='alert'>
-        Elimindo
-      </div>";
+        Eliminado
+      </div>";     
     $con->close();
+    echo "<meta http-equiv=refresh content=0;URL=autores.php>";
     }
 }
 if(isset($_POST["actualiza"])){ //Actualizar Actor
@@ -266,6 +378,8 @@ if(mysqli_query($con,$updat)){
         Actualizado
       </div>";
     $con->close();
+    echo "<meta http-equiv=refresh content=0;URL=autores.php>";
+
 }
 else{
     echo "error";
@@ -287,21 +401,33 @@ if(isset($_POST["b1"]) && !empty($_POST["b1"])) { //Agregar Autor
     $nacio= $_POST['Nacionalidad'];
     $apa=$_POST['Apellido'];
     $age=$_POST['edad'];
+    if(empty($nombre)){echo "<div class='alert alert-primary' role='alert' style='background-color:red'>Falta el nombre del autor</div>";}
+    if(empty($nacio)){echo "<div class='alert alert-primary' role='alert'> Falta la nacionalidad del autor </div>";}
+    if(empty($apa)){echo "<div class='alert alert-primary' role='alert'> Falta el apellido del autor </div>";}
+    if(empty($age)){echo "<div class='alert alert-primary' role='alert'> Falta la edad del autor</div>";
+      }else{
+        if($age<15){ echo "<div class='alert alert-primary' role='alert'> Edad no aceptada para un autor </div>";
+        }
+    if(!empty($nombre) && !empty($nacio) && !empty($apa) && !empty($age) && $age>15){
         $sql1="INSERT INTO autor (id_autor,NombreAutor,ApellidosAutor,Nacionalidad,Edad) VALUES ($id,'$nombre','$apa','$nacio','$age')";
-    if(mysqli_query($con,$sql1)){
-        echo "<div class='alert alert-success' role='alert'>
-        Agregado Correctamente
-      </div>";
-        $nombre="";
-        $nacio="";
-        $apa="";
-        $age="";
-        unset($_POST);
-        $con->close();
-    }
-    else{
-        echo "Error: " . $sql . "<br>" . mysqli_error($con);
-    }
+        if(mysqli_query($con,$sql1)){
+            echo "<div class='alert alert-success' role='alert'>
+            Agregado Correctamente
+          </div>";
+            $nombre="";
+            $nacio="";
+            $apa="";
+            $age="";
+            unset($_POST);
+            $con->close();
+            echo "<meta http-equiv=refresh content=0;URL=autores.php>";
+        }
+        else{
+            echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        }
+      }     
+      }
+        
 }
 ?>
 <script>
